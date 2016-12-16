f2cb1c6cb329b9f62661a8c84e889b8d22a10bd4
Uyen Lam <uyenlam@Uyens-MacBook.local>
2016-12-16 05:11:25 -0600

    Added log.txt function

--- liri.js
+++ liri.js
@@ -3,6 +3,8 @@ var twitter = require('twitter');
 var spotifyAPI = require('spotify-web-api-node');
 var request = require('request');
 var inquirer = require('inquirer');
+// var rottenAPI = require('rotten-tomatoes-api')('j6ahmr555pen2ybkb7nsxbb9');
+// var util = require('util');
 
 // used to read files later
 var fs = require('fs');
@@ -25,29 +27,43 @@ var spotify = new spotifyAPI({
 })
 
 // take in the arguments
+var commandline = '';
+    for (var i = 2; i < process.argv.length; i++){
+        var b = process.argv[i];
+        commandline += b + ' ';
+    }
 var action = process.argv[2];
-var itemName = process.argv[3];
+var itemName = ''; //this is a string
+    for (var i = 3; i < process.argv.length; i++) {
+        var a = process.argv[i];
+        itemName += a + ' ';
+    }
 
-
-// MAIN ACTION=========================================
-
-switch (action) {
-    case "my-tweets":
-        readTweets();
-        break;
-    case "spotify-this-song":
-        findSong();
-        break;
-    case "movie-this":
-        findMovie();
-        break;
-    case "do-what-it-says":
-        doIt();
-        break;
+// FUNCTIONS===========================================
+// main command
+function mainCommand(){
+    fs.appendFile('log.txt','Command line: ' + commandline + '\n',function(err){
+        if (err) return console.log('Error occured: ' + err);
+        console.log('This command line has been updated in log.txt.')
+        console.log("====================");
+    })
+    switch (action) {
+        case "my-tweets":
+            readTweets();
+            break;
+        case "spotify-this-song":
+            findSong();
+            break;
+        case "movie-this":
+            findMovie();
+            break;
+        case "do-what-it-says":
+            doIt();
+            break;
+    }
 }
 
 
-// FUNCTIONS===========================================
 // handling twitter
 function readTweets() {
 
@@ -68,17 +84,11 @@ function readTweets() {
 
 // handling spotify
 function findSong() {
-    var songName = ''; //this is a string
-    for (var i = 3; i < process.argv.length; i++) {
-        var a = process.argv[i];
-        songName += a + ' ';
-    }
 
     var searchResults = [];
 
-
-    if (songName) {
-        spotify.searchTracks(songName, { limit: 5, offset: 0 }).then(function(data) {
+    if (itemName) {
+        spotify.searchTracks(itemName, { limit: 5, offset: 0 }).then(function(data) {
             for (var i = 0; i < 5; i++) {
                 searchResults.push(data.body.tracks.items[i].name + ' by ' + data.body.tracks.items[i].artists[0].name);
             }
@@ -122,22 +132,54 @@ function findSong() {
 
 }
 
-// function findMovie() {
-//     // Then run a request to the OMDB API with the movie specified
-//     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&r=json";
+function findMovie() {
 
-//     // This line is just to help us debug against the actual URL.
-//     console.log(queryUrl);
+    // Then run a request to the OMDB API with the movie specified
+    var queryUrl = "http://www.omdbapi.com/?t=" + itemName + "&y=&plot=short&r=json";
 
-//     request(queryUrl, function(error, response, body) {
+    function search(){
+       
+        request(queryUrl, function(error, response, body) {
+            // If the request is successful
+            if (!error && response.statusCode === 200) {
 
-//         // If the request is successful
-//         if (!error && response.statusCode === 200) {
+                // Parse the body of the site and recover just the imdbRating
+                // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+                console.log("====================");
+                console.log("Movie title: " + JSON.parse(body).Title);
+                console.log("Release Year: " + JSON.parse(body).Year);
+                console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+                console.log("Release Year: " + JSON.parse(body).Year);
+                console.log("Country: " + JSON.parse(body).Country);
+                console.log("Language " + JSON.parse(body).Language);
+                console.log("Plot: " + JSON.parse(body).Plot);
+                console.log("Actors: " + JSON.parse(body).Actors);
+                console.log("====================");
+            } 
 
-//             // Parse the body of the site and recover just the imdbRating
-//             // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-//             console.log("Release Year: " + JSON.parse(body).Year);
-//         }
-//     });
+        });
+    }
 
-// }
+    if (itemName){
+        search();
+
+    } else{
+        var itemName = 'Mr. Nobody'
+        var queryUrl = "http://www.omdbapi.com/?t=" + itemName + "&y=&plot=short&r=json";
+        search();
+    }
+    
+}
+
+function doIt(){
+    fs.readFile('random.txt','utf8',function(err, data){
+        if(err) return console.log(err);
+        console.log(data);
+        action = data.split(',')[0];
+        itemName = data.split(',')[1];
+        mainCommand();
+    })
+}
+
+// MAIN ACTION=========================================
+mainCommand();
